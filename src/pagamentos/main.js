2a456f15f10d3b4af88caa6a82476f45f3d7d1f2
thiagoamim <thiagoamim11@gmail.com>
2022-10-23 17:27:04 -0300

    criando arquivo main e input de pagamento

---
+++ src/pagamentos/main.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Reactnp from 'react';
+import React from 'react';
+import Pagamentos from './src/pagamentos/pagamentos';
+
+
+export default function Main() {
+    return(
+
+        <View><Pagamentos/></View>
+    );
+}
+
--- src/pagamentos/pagamentos.js
+++ src/pagamentos/pagamentos.js
@@ -1,13 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, SafeAreaView, FlatList } from 'react-native';
 import React from 'react';
+
+
+
 
 
 const Pagamentos = () => {
     return (
-        <Text>Digite o codigo</Text>
+        <SafeAreaView style={estilos.container}>
+            <Text style={estilos.texto}>Vendedor: </Text>
+            <TextInput style={estilos.input} placeholder='Fulano'></TextInput>            
+            <Text style={estilos.texto}>Cliente </Text>
+            <TextInput style={estilos.input} placeholder='Alterar'></TextInput> 
+            
+            
+            
+          </SafeAreaView>
+
+          
+        
     );
 
 }
 
 export default Pagamentos;
+
+
+const estilos = StyleSheet.create ({
+    container: {
+        paddingTop: 30,
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'flex-start',
+        height: 50,
+        flexDirection: 'row',
+        marginTop: 40,
+        
+        
+    },
+
+    input: {
+        fontSize: 15,
+        marginRight: 120,
+    },
+
+    texto: {
+        marginLeft: 15
+    }
+})
+
